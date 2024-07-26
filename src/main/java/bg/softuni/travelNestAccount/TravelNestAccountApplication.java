9349164c0891ce5be75ef13d90dbdd91b9dcc1df
Rosen <roskonenov@gmail.com>
2024-07-26 17:42:47 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/travelNestAccount/TravelNestAccountApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.travelNestAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelNestAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelNestAccountApplication.class, args);
+	}
+
+}
